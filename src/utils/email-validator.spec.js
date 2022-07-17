7bcf412ef3192cf1b201b31a6b06884eddc5be29
Joel Fragoso <joelfragoso85@icloud.com>
2022-07-17 19:07:31 -0300

    test: adiciona o makeSut para criação do EmailValidator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Deve retornar verdadeiro se o Validator retornar verdadeiro', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('johndoe@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Deve retornar falso se o Validator retornar falso', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid@email.com')
     expect(isEmailValid).toBe(false)
   })
